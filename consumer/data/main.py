b2c2111d31af7c2f2f6796a98816c0ac630bbf9a
João Gabriel L. Romão <gabriel.lacerda@engenharia.ufjf.br>
2022-10-27 11:11:45 -0300

    new plots

--- consumer/data/main.py
+++ consumer/data/main.py
@@ -2,15 +2,36 @@
 from os import listdir
 from pathlib import Path
 from pprint import pprint
-from random import randint, random
-from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
 
 DATA_DIR = Path(__file__).parent
 
 
-def plot_time(time: list):
+def plot_average(kafka_averages: np.ndarray, rabbitmq_averages: np.ndarray):
+
+    max_tests = min([len(rabbitmq_averages), len(kafka_averages)])
+    rabbitmq_averages = rabbitmq_averages[:max_tests]
+    kafka_averages = kafka_averages[:max_tests]
+
+    packets = list(range(max_tests))
+    rmq_color = (0, 0.5, 0)
+    kafka_color = (0.5, 0, 0)
+    plt.scatter(packets, rabbitmq_averages, color=rmq_color, alpha=0.5)
+    plt.plot(packets, rabbitmq_averages.mean() *
+             np.ones((len(packets))), color=rmq_color)
+    plt.scatter(packets, kafka_averages, color=kafka_color, alpha=0.5)
+    plt.plot(packets, kafka_averages.mean() *
+             np.ones((len(packets))), color=kafka_color)
+    plt.legend(["RabbitMQ", "Média RabbitMQ", "Kafka", "Média Kafka"])
+    plt.xlabel("Teste")
+    plt.ylim(0)
+    plt.ylabel("Tempo (us)")
+    plt.savefig(DATA_DIR / "compare_evarages.png")
+    plt.show()
+
+
+def plot_time(time: list, broker_type: str):
 
     packets = list(range(len(time)))
     color = (0, 0.5, 0)
@@ -20,21 +41,22 @@ def plot_time(time: list):
     plt.xlabel("Packet")
     plt.ylim(0)
     plt.ylabel("Time (us)")
-    plt.savefig(DATA_DIR / "figure.png")
+    plt.savefig(DATA_DIR / f"best_time_{broker_type}.png")
     plt.show()
+
 
 def main():
     # print(matplotlib.get_backend())
 
-    kafka_times: List[List[int]] = []
-    rabbitmq_times: List[List[int]] = []
+    kafka_times: list[np.uint] = []
+    rabbitmq_times: list[np.uint] = []
 
     times_files = listdir(DATA_DIR / 'times_database')
     for file_name in times_files:
 
         with open(DATA_DIR / 'times_database' / file_name, encoding='utf-8') as file:
             lines = file.readlines()
-            times = [line.rstrip() for line in lines]
+            times = np.array([int(line.rstrip()) for line in lines])
             times = np.uint(times)
             if file_name.startswith('kafka'):
                 kafka_times.append(times)
@@ -44,13 +66,14 @@ def main():
     kafka_averages = np.uint([times.mean() for times in kafka_times])
     kafka_worst = kafka_averages.argmax()
     kafka_best = kafka_averages.argmin()
-    plot_time(kafka_times[kafka_best])
-    
+    # plot_time(kafka_times[kafka_best], "kafka")
+
     rabbitmq_averages = np.uint([times.mean() for times in rabbitmq_times])
     rabbitmq_worst = rabbitmq_averages.argmax()
     rabbitmq_best = rabbitmq_averages.argmin()
 
-    plot_time(rabbitmq_times[rabbitmq_best])
+    # plot_time(rabbitmq_times[rabbitmq_best], "rabbitqm")
+    plot_average(kafka_averages, rabbitmq_averages)
     return
 
     # packets = list(range(len(times)))
@@ -62,39 +85,6 @@ def main():
     # plt.scatter(packets, rabbitmq_times[rabbitmq_worst], color=color, alpha=0.5)
 
     # plt.legend(["Best", "Worst"])
-
-    max_tests = min([len(rabbitmq_averages), len(kafka_averages)])
-    rabbitmq_averages = rabbitmq_averages[:max_tests]
-    kafka_averages = kafka_averages[:max_tests]
-
-    packets = list(range(max_tests))
-    rmq_color = (0, 0.5, 0)
-    kafka_color = (0.5, 0, 0)
-    plt.scatter(packets, rabbitmq_averages, color=rmq_color, alpha=0.5)
-    plt.plot(packets, rabbitmq_averages.mean() * np.ones((len(packets))), color=rmq_color)
-    plt.scatter(packets, kafka_averages, color=kafka_color, alpha=0.5)
-    plt.plot(packets, kafka_averages.mean() * np.ones((len(packets))), color=kafka_color)
-    plt.legend(["RabbitMQ", "Média RabbitMQ", "Kafka", "Média Kafka"])
-    plt.xlabel("Teste")
-    plt.ylim(0)
-
-    # for times in kafka_times:
-    #     packets = list(range(len(times)))
-    #     plt.scatter(packets, times, color='steelblue', alpha=0.5)
-    # for times in rabbitmq_times:
-    #     packets = list(range(len(times)))
-    #     color = (0, random(), 0)
-    #     plt.scatter(packets, times, color=color, alpha=0.5)
-
-    # plt.plot(x, kafka_times.mean() * np.ones((len(x))), color='blue')
-    # plt.scatter(x, rabbitmq_times, color="greenyellow")
-    # plt.plot(x, rabbitmq_times.mean() * np.ones((len(x))), color='green')
-    # plt.xlabel("Pacote")
-    plt.ylabel("Tempo (us)")
-    # plt.legend(["Kafka", "Média Kafka", "RabbitMQ", "Média RabbitMQ"])
-    # plt.legend(["Kafka", "Média Kafka", "RabbitMQ", "Média RabbitMQ"])
-    plt.savefig(DATA_DIR / "figure.png")
-    plt.show()
 
 
 if __name__ == "__main__":
